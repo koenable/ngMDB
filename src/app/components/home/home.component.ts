ef6e7b3e62277d6070493cb3f3433737f51378ad
baholo <mokoenabaholo90@gmail.com>
2020-06-17 20:57:35 +0200

    Home component has NgForm that validate user's email and password. Returns form.value on submitt

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { User } from './../../shared/models/user';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -14,12 +15,21 @@ export class HomeComponent implements OnInit {
     // this.countrServ.getCountryData();
   }
 
+  // tips = [1,2,3,4]
+  tips = [
+    "Settle arguments with words, not fists or weapons",
+    "Learn safe routes for walking in the neighborhood, and know good places to seek help",
+    "Report any crimes or suspicious actions to the police, school authorities, and parents",
+    "Don't open the door to anyone you and your parents don't know and trust."]
 
-  model = new User('Json','json@work','nonOfYourBuz');
+  model = new User('','','');
 
   submitted = false;
 
-  onSubmit() { this.submitted = true; }
+  onSubmit(form?:NgForm){
+      console.log(form.value);
+     this.submitted = true; 
+  }
 
   newHero() {
     this.model = new User('', '', '');
