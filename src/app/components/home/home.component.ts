75f860b31c9ee365fea743480e369d27d4fb9b44
Baholo <mokoenabaholo90@gmail.com>
2020-06-18 08:09:06 +0200

    Http request method added to project.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
+import { AuthService } from './../../shared/services/auth.service';
 import { User } from './../../shared/models/user';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-home',
@@ -9,10 +11,10 @@ import { NgForm } from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
 
- constructor(){}
+ constructor(private authServ: AuthService){}
 
   ngOnInit() { 
-    // this.countrServ.getCountryData();
+ 
   }
 
   // tips = [1,2,3,4]
@@ -22,18 +24,18 @@ export class HomeComponent implements OnInit {
     "Report any crimes or suspicious actions to the police, school authorities, and parents",
     "Don't open the door to anyone you and your parents don't know and trust."]
 
-  model = new User('','','');
+  model = new User('','');
 
   submitted = false;
 
-  onSubmit(form?:NgForm){
-      console.log(form.value);
-     this.submitted = true; 
-  }
+  // When user clicks the login button
+  onSubmit(form:NgForm){
+      this.model = new User(form.value.email, form.value.password);
+      // user the auth service to make a post request    
+      this.authServ.login(this.model)
+    }
 
-  newHero() {
-    this.model = new User('', '', '');
-  }
+
   
  
 }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,9 +1,21 @@
+import { User } from './../models/user';
 import { Injectable } from '@angular/core';
+import { HttpService } from './http.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpService) { }
+
+  URL: 'localhost:300/login';
+
+  login(details:User){
+      this.http.postdata(this.URL,details).subscribe((data)=>{
+            if(data) { return console.log("failed" + data) }
+      })
+  }
+
 }
